0548fb5765ea1bb2ea35a51606e7166947c8187f
Cipher413 <48688369+Cipher413@users.noreply.github.com>
2019-03-21 19:50:06 +0100

    Update library.js

--- library.js
+++ library.js
@@ -7,13 +7,5 @@ function writeadd(id, doc){document.getElementById(id).innerHTML+=(doc)}
 function getRandomInt(max) {return Math.floor(Math.random() * Math.floor(max));}
 function factorial(n){if(n===0){return 1}else m=n;while(m>1){m--;n*=m;}return n}
 function divisors(n){var i=1;while (i<=n){if(n%i===0){document.writeln(i)};i++}}
-function setCookie(key, value) {
-            var expires = new Date();
-            expires.setTime(expires.getTime() + (1 * 24 * 60 * 60 * 1000));
-            document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
-        }
-
-        function getCookie(key) {
-            var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
-            return keyValue ? keyValue[2] : null;
-        }
+function setCookie(key, value) {var expires = new Date();expires.setTime(expires.getTime() + (1 * 24 * 60 * 60 * 1000));document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();}
+function getCookie(key) {var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');return keyValue ? keyValue[2] : null;}
